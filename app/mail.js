f5d8b186413bb09b9feef7465dca674bbafd3053
Jason Stirnaman <stirnamanj@gmail.com>
2016-10-30 18:10:54 -0500

    Tour of Heroes displaying hero name in form input. Adds FormsModule.

--- app/mail.js
+++
@@ -1,8 +0,0 @@
-// Initializes the platform that your application runs in, then uses
-// the platform to bootstrap your AppModule.
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=mail.js.map
